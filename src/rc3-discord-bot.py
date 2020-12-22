9b6b136ea775296167ee51a4dcbfffb021149959
Jan Feil <11638228+jfeil@users.noreply.github.com>
2020-12-23 00:42:38 +0100

    Move rc3-discord-bot

--- src/rc3-discord-bot.py
+++ src/rc3-discord-bot.py
@@ -1,4 +1,7 @@
 import argparse
+import datetime
+
+from schedule_planner import SchedulePlanner 
 
 json_source = 'https://data.c3voc.de/rC3/everything.schedule.json'
 
@@ -10,3 +13,5 @@ parser.add_argument('-u', '--update-rate', dest='rate', required=False, default=
 if __name__ == "__main__":
     args = parser.parse_args()
     print(args.token)
+    schedule_planner = SchedulePlanner(json_source)
+    schedule_planner.current_events(datetime.datetime.fromtimestamp(1609104000, tz=datetime.timezone(datetime.timedelta(seconds=3600))))

124b6431257d255ddc6421158eb088979ba02f31
Jan Feil <11638228+jfeil@users.noreply.github.com>
2020-12-27 23:14:50 +0100

    Change main.py to support token from config

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from rc3_discord_bot import DiscordBot
 json_source = 'https://data.c3voc.de/rC3/everything.schedule.json'
 
 parser = argparse.ArgumentParser('This is a bot for the rC3 program!')
-parser.add_argument('-t', '--token', dest='token', required=True, help='This is the *required* discord bot api token!')
+parser.add_argument('-t', '--token', dest='token', required=False, help='This is the *required* discord bot api token!', default=None)
 parser.add_argument('-u', '--update-rate', dest='rate', required=False, default=3600)
 
 
